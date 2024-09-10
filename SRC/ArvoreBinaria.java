bf3aedbfee758bb024e664381b598dac24da220e
Felipe-N-Fonseca <felipenasfonseca@gmail.com>
2024-09-10 20:08:57 -0300

    Revert "testw"
    This reverts commit 91edea7385d50c0d5a13776833bacf4a56548dfc.

--- SRC/ArvoreBinaria.java
+++ SRC/ArvoreBinaria.java
@@ -8,12 +8,8 @@ class ArvoreBinaria{
         arvore.getRoot().getRight().setRight(new Node("F", arvore.getRoot().getRight(), null, null));
 
 
-        System.out.println("\n-----------------------------------\n");
-
         // Informações dos nós
         System.out.println("\nInformações dos nós:");
-
-        System.out.println("\n-----------------------------------\n");
 
         // Informações de A
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
@@ -23,8 +19,6 @@ class ArvoreBinaria{
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
 
-        System.out.println("\n-----------------------------------\n");
-
         // Informações de B
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
         System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
@@ -32,8 +26,6 @@ class ArvoreBinaria{
         System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
-
-        System.out.println("\n-----------------------------------\n");
 
         // Informações de C
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
@@ -43,8 +35,6 @@ class ArvoreBinaria{
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
 
-        System.out.println("\n-----------------------------------\n");
-
         // Informações de D
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
         System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
@@ -52,8 +42,6 @@ class ArvoreBinaria{
         System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
-
-        System.out.println("\n-----------------------------------\n");
 
         // Informações de E
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
@@ -63,8 +51,6 @@ class ArvoreBinaria{
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
 
-        System.out.println("\n-----------------------------------\n");
-
         // Informações de F
         System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
         System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
@@ -73,46 +59,29 @@ class ArvoreBinaria{
         System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
         System.out.println();
 
-        System.out.println("\n-----------------------------------\n");
 
         // Informações da arvore
-        System.out.println("\nInformações da árvore:");
+        System.out.println("\nInformações da arvore:");
 
-        System.out.println("\n-----------------------------------\n");
-        
         // Se está vazia
-        System.out.println("A árvore está vazia: " + arvore.isEmpty());
-        System.out.println("\n-----------------------------------\n");
-        
+
+
         // Grau
-        System.out.println("O grau da árvore é: " + arvore.getDegree(arvore.getRoot()));
-        System.out.println("\n-----------------------------------\n");
-        
+
+
         // Altura
-        System.out.println("A altura da árvore é de: " + arvore.getHeight());
-        System.out.println("\n-----------------------------------\n");
-        
+
+
         //Percurso em ordem
-        System.out.println("Percorrendo de forma em ordem:");
-        arvore.inOrderTraversal(arvore.getRoot());
-        System.out.println("\n-----------------------------------\n");
-        
+
+
         //Percurso pré ordem
-        System.out.println("Percorrendo de forma pré ordem:");
-        arvore.preOrderTraversal(arvore.getRoot());
-        System.out.println("\n-----------------------------------\n");
-        
-        
+
+
         //Percurso pós ordem
-        System.out.println("Percorrendo de forma pós ordem:");
-        arvore.postOrderTraversal(arvore.getRoot());
-        System.out.println("\n-----------------------------------\n");
-        
+
         
         // Percorrendo por nível 
-        System.out.println("Percorrendo através dos níveis:");
-        arvore.levelOrderTraversal();
-        System.out.println("\n-----------------------------------\n");
 
 
         
