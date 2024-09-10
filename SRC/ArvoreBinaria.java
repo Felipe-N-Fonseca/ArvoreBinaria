a5523cdd141fe8e0eb876178e879b8f6d8ff9734
Felipe-N-Fonseca <felipenasfonseca@gmail.com>
2024-09-10 20:09:52 -0300

    Revert "Criados todas as classes necessárias"
    This reverts commit 3fd77486431e3b10a6f116d6a67b3d2c7fd8bcf3.

--- SRC/ArvoreBinaria.java
+++ SRC/ArvoreBinaria.java
@@ -1,89 +1,30 @@
 class ArvoreBinaria{
     public static void main(String[] args) {
-        BinaryTree arvore = new BinaryTree(new Node("A", null, null, null));
-        arvore.getRoot().setLeft(new Node("B", arvore.getRoot(), null, null));
-        arvore.getRoot().getleft().setLeft(new Node("D", arvore.getRoot().getleft(), null, null));
-        arvore.getRoot().setRight(new Node("C", arvore.getRoot(), null, null));
-        arvore.getRoot().getRight().setLeft(new Node("E", arvore.getRoot().getRight(), null, null));
-        arvore.getRoot().getRight().setRight(new Node("F", arvore.getRoot().getRight(), null, null));
+        Node root = new Node();
+        root.setLeft(new Node());
+        root.getleft().setParent(root);
+        root.setRight(new Node());
+        root.getRight().setParent(root);
+        root.getRight().setRight(new Node());
+        root.getRight().getRight().setParent(root.getRight());
+        root.getRight().setLeft(new Node());
+        root.getRight().getleft().setParent(root.getRight());
+        root.getRight().getRight().setRight(new Node());
+        root.getRight().getRight().getRight().setParent(root.getRight().getRight());
 
+        System.out.println("O root é raiz: " + root.isRoot());
+        System.out.println("O filho direito do root é raiz: " + root.getRight().isRoot());
+        System.out.println("O root é folha: " + root.isLeaf());
+        System.out.println("O filho esquerdo do root é folha: " + root.getleft().isLeaf());
+        System.out.println("O grau do root é: " + root.getDegree());
+        System.out.println("O grau filho esquerdo do root é: " + root.getleft().getDegree());
+        System.out.println("O nível do root é: " + root.getLevel());
+        System.out.println("O nível do neto mais a direita do root é: " + root.getRight().getRight().getLevel());
+        System.out.println("A altura da arvore é: " + root.getHeight());
 
-        // Informações dos nós
-        System.out.println("\nInformações dos nós:");
-
-        // Informações de A
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-        // Informações de B
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-        // Informações de C
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-        // Informações de D
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-        // Informações de E
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-        // Informações de F
-        System.out.println("O 'A' é raiz: " + arvore.getRoot().isRoot());
-        System.out.println("O 'A' é folha: " + arvore.getRoot().isLeaf());
-        System.out.println("O grau de 'A' é: " + arvore.getRoot().getDegree());
-        System.out.println("O nível de 'A' é: " + arvore.getRoot().getLevel());
-        System.out.println("A altura do nó 'A' é: " + arvore.getRoot().getHeight());
-        System.out.println();
-
-
-        // Informações da arvore
-        System.out.println("\nInformações da arvore:");
-
-        // Se está vazia
-
-
-        // Grau
-
-
-        // Altura
-
-
-        //Percurso em ordem
-
-
-        //Percurso pré ordem
-
-
-        //Percurso pós ordem
-
-        
-        // Percorrendo por nível 
-
-
-        
     }
+
+    
+
+
 }
